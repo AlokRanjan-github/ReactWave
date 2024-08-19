b1e41ba4f4c90da8f1f8c302eed65e501e3a6a9a
AlokRanjan-github <alokrp7007@gmail.com>
2024-08-19 22:17:35 +0530

    noth

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,11 +6,11 @@ import Button from "./Button";
 const Hero = () => {
   return (
     <Section
-    className="pt-[12rem] -mt-[5.25]"
-    crosses
-    crossOffset="lg:translate-y-[5.25rem]"
-    customPaddings
-    id="hero"
+      className="pt-[12rem] -mt-[5.25]"
+      crosses
+      crossOffset="lg:translate-y-[5.25rem]"
+      customPaddings
+      id="hero"
     >
       <div className="container relative">
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
